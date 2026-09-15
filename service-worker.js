ae3d635e67566351279e1431edcb33bf90b2c703
Habsco Sadaqah Jariyah Development Trust <70951857+HabscoSadaqah@users.noreply.github.com>
2026-09-15 18:29:53 +0000

    perf: safely minify browser assets

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,1 @@
-/* Habsco service worker: deliberately disabled. Financial/member data must never be cached client-side. */
-self.addEventListener("install", event => {
-  event.waitUntil(self.skipWaiting());
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil((async () => {
-    try {
-      const keys = await caches.keys();
-      await Promise.all(keys.map(key => caches.delete(key)));
-      await self.clients.claim();
-      await self.registration.unregister();
-    } catch (_) {}
-  })());
-});
+self.addEventListener("install",event=>{event.waitUntil(self.skipWaiting())}),self.addEventListener("activate",event=>{event.waitUntil((async()=>{try{const keys=await caches.keys();await Promise.all(keys.map(key=>caches.delete(key))),await self.clients.claim(),await self.registration.unregister()}catch(_){}})())});
